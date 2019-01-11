f5de82409b268eadb5897f7080e3fcfc7c830af3
Djordje Vukcevic <vukcevicdjordjemne@gmail.com>
2019-01-11 19:00:53 +0100

    [YB Constants] Joint Position thresholds added

--- Controller/include/youbot_constants.hpp
+++ Controller/include/youbot_constants.hpp
@@ -36,55 +36,58 @@ SOFTWARE.
 #include <fstream>
 #include <cmath>
 
+#define PI 3.14159265358979323846
+#define DEG_TO_RAD(x) (x) * PI / 180.0
+
 namespace youbot_constants
 {
     // Number of joints in the manipulator
-    const int NUMBER_OF_JOINTS_(5);
-    const int NUMBER_OF_SEGMENTS_(5);
-    const int NUMBER_OF_FRAMES_(6);
+    const int NUMBER_OF_JOINTS(5);
+    const int NUMBER_OF_SEGMENTS(5);
+    const int NUMBER_OF_FRAMES(6);
 
     //Arm's root acceleration
-    const std::vector<double> root_acceleration_ {0.0, 0.0, 9.81, 0.0, 0.0, 0.0};
+    const std::vector<double> root_acceleration {0.0, 0.0, 9.81, 0.0, 0.0, 0.0};
 
     //Kuka youBot store position limit values: positive and negative
-    const std::vector<double> joint_position_limits_max_1_ {  2.9496,  1.5707,  2.5481,  1.7889,  2.9234};
-    const std::vector<double> joint_position_limits_min_1_ { -2.9496, -1.1344, -2.6354, -1.7889, -2.9234};
+    const std::vector<double> joint_position_limits_max_1 {  2.9496,  1.5707,  2.5481,  1.7889,  2.9234};
+    const std::vector<double> joint_position_limits_min_1 { -2.9496, -1.1344, -2.6354, -1.7889, -2.9234};
 
     //Position limit values from URDF file: positive and negative
-    const std::vector<double> joint_position_limits_max_2_ {5.89921, 2.70526,  0.00000, 3.57792, 5.84685};
-    const std::vector<double> joint_position_limits_min_2_ {0.00000, 0.00000, -5.16617, 0.00000, 0.00000};
+    const std::vector<double> joint_position_limits_max_2 {5.89921, 2.70526,  0.00000, 3.57792, 5.84685};
+    const std::vector<double> joint_position_limits_min_2 {0.00000, 0.00000, -5.16617, 0.00000, 0.00000};
     
-    const std::vector<double> joint_position_soft_threshold {0.00000, 0.00000, -5.16617, 0.00000, 0.00000};
+    const std::vector<double> joint_position_thresholds {DEG_TO_RAD(15), DEG_TO_RAD(15), DEG_TO_RAD(12), DEG_TO_RAD(10), DEG_TO_RAD(7)};
 
     // Robocup URDF file parameters for velocity limits
-    // std::vector<double> joint_velocity_limits_ {1.5707, 0.8, 1.0, 1.5707, 1.5707};
+    // std::vector<double> joint_velocity_limits {1.5707, 0.8, 1.0, 1.5707, 1.5707};
 
     // YouBot Store velocity limits
-    const std::vector<double> joint_velocity_limits_ {1.5707, 01.5707, 1.5707, 1.5707, 1.5707};
+    const std::vector<double> joint_velocity_limits {1.5707, 01.5707, 1.5707, 1.5707, 1.5707};
 
     // JP's max torques
-    // std::vector<double> joint_torque_limits_ {12.9012, 12.9012, 8.2700, 4.1748, 1.7550};
+    // std::vector<double> joint_torque_limits {12.9012, 12.9012, 8.2700, 4.1748, 1.7550};
 
     // youBot store's max torques 
-    const std::vector<double> joint_torque_limits_ {9.5, 9.5, 6.0, 2.0, 1.0};
+    const std::vector<double> joint_torque_limits {9.5, 9.5, 6.0, 2.0, 1.0};
 
     // Benjamin Keiser' max torques (fast version)
-    // std::vector<double> joint_torque_limits_ {17.0, 17.0, 8.0, 2.7, 1.0}};
+    // std::vector<double> joint_torque_limits {17.0, 17.0, 8.0, 2.7, 1.0}};
 
     // Offsets required for the youBot store model: Negative Candle config values -Robocup
-    // std::vector<double> youbot_joint_offsets_ {-2.1642, -1.13446, 2.54818, -1.78896, -2.9234};
+    // std::vector<double> youbot_joint_offsets {-2.1642, -1.13446, 2.54818, -1.78896, -2.9234};
 
     // Offsets required for the youBot store model: Negative Candle config values - JP 
-    const std::vector<double> joint_offsets_ {-2.94960, -1.13446, 2.54818, -1.78896, -2.9234};
+    const std::vector<double> joint_offsets {-2.94960, -1.13446, 2.54818, -1.78896, -2.9234};
 
     // Offsets required for the youBot store model: Negative Candle config values -Sven's
-    // std::vector<double> joint_offsets_ {-2.9496, -1.1344, 2.6354, -1.7890, -2.9234};
+    // std::vector<double> joint_offsets {-2.9496, -1.1344, 2.6354, -1.7890, -2.9234};
 
     // Offsets required for the youBot store model: Negative Candle config values: keiser's
-    // std::vector<double> joint_offsets_ {-2.9496, -1.1344, 2.5481, -1.7889, -2.9234};
+    // std::vector<double> joint_offsets {-2.9496, -1.1344, 2.5481, -1.7889, -2.9234};
 
     // Rotor inertia - "d" in the algorithm: Computed from youBot store values
-    const std::vector<double> joint_inertia_ {0.33848, 0.33848, 0.13571, 0.04698, 0.01799};
+    const std::vector<double> joint_inertia {0.33848, 0.33848, 0.13571, 0.04698, 0.01799};
 }
 
 #endif /* YOUBOT_CONSTANTS_HPP */
