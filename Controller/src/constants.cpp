0973488dbb6ea89d28b11b6c299431bb18fbcd09
Djordje Vukcevic <vukcevicdjordjemne@gmail.com>
2019-01-23 16:49:56 +0100

    [YB Constrants] Joint Torque Limits updated

--- Controller/src/constants.cpp
+++ Controller/src/constants.cpp
@@ -29,44 +29,48 @@ namespace youbot_constants
     const std::vector<double> joint_position_limits_max_2_sim { 5.899210,  2.705260,  0.000001,  3.577920,  5.846850};
     const std::vector<double> joint_position_limits_min_2_sim {-0.000001, -0.000001, -5.166170, -0.000001, -0.000001};
 
-    const std::vector<double> joint_position_thresholds {DEG_TO_RAD(0), DEG_TO_RAD(0), DEG_TO_RAD(0), DEG_TO_RAD(0), DEG_TO_RAD(0)};
-    // const std::vector<double> joint_position_thresholds {DEG_TO_RAD(10), DEG_TO_RAD(10), DEG_TO_RAD(10), DEG_TO_RAD(8), DEG_TO_RAD(5)};
+   //  const std::vector<double> joint_position_thresholds {DEG_TO_RAD(0), DEG_TO_RAD(0), DEG_TO_RAD(0), DEG_TO_RAD(0), DEG_TO_RAD(0)};
+    const std::vector<double> joint_position_thresholds {DEG_TO_RAD(15), DEG_TO_RAD(15), DEG_TO_RAD(10), DEG_TO_RAD(10), DEG_TO_RAD(8)};
 
     // Robocup URDF file parameters for velocity limits
-    // std::vector<double> joint_velocity_limits {1.5707, 0.8, 1.0, 1.5707, 1.5707};
+    // const std::vector<double> joint_velocity_limits {1.5707, 0.8, 1.0, 1.5707, 1.5707};
 
     // YouBot Store velocity limits
     const std::vector<double> joint_velocity_limits {1.5707, 01.5707, 1.5707, 1.5707, 1.5707};
 
     // JP's max torques
-    // std::vector<double> joint_torque_limits {12.9012, 12.9012, 8.2700, 4.1748, 1.7550};
+   //  const std::vector<double> joint_torque_limits {12.9012, 12.9012, 8.2700, 4.1748, 1.7550};
 
-    // youBot store's max torques 
-    const std::vector<double> joint_torque_limits {9.5, 9.5, 6.0, 2.0, 1.0};
+   //  // youBot store's max torques 
+   //  const std::vector<double> joint_torque_limits {9.5, 9.5, 6.0, 2.0, 1.0};
+
+    // custom max torques 
+    const std::vector<double> joint_torque_limits {12.9012, 12.9012, 8.0, 2.7, 1.2};
 
     // Benjamin Keiser' max torques (fast version)
-    // std::vector<double> joint_torque_limits {17.0, 17.0, 8.0, 2.7, 1.0}};
+    // const std::vector<double> joint_torque_limits {17.0, 17.0, 8.0, 2.7, 1.0}};
 
     // Offsets required for the youBot store model: Negative Candle config values -Robocup
-    // std::vector<double> youbot_joint_offsets {-2.1642, -1.13446, 2.54818, -1.78896, -2.9234};
+    // const std::vector<double> youbot_joint_offsets {-2.1642, -1.13446, 2.54818, -1.78896, -2.9234};
 
     // Offsets required for the youBot store model: Negative Candle config values - JP 
     const std::vector<double> joint_offsets {-2.94960, -1.13446, 2.54818, -1.78896, -2.9234};
 
     // Offsets required for the youBot store model: Negative Candle config values -Sven's
-    // std::vector<double> joint_offsets {-2.9496, -1.1344, 2.6354, -1.7890, -2.9234};
+    // const std::vector<double> joint_offsets {-2.9496, -1.1344, 2.6354, -1.7890, -2.9234};
 
     // Offsets required for the youBot store model: Negative Candle config values: keiser's
-    // std::vector<double> joint_offsets {-2.9496, -1.1344, 2.5481, -1.7889, -2.9234};
+    // const std::vector<double> joint_offsets {-2.9496, -1.1344, 2.5481, -1.7889, -2.9234};
 
     // Rotor inertia - "d" in the algorithm: Computed from youBot store values
     const std::vector<double> joint_inertia {0.33848, 0.33848, 0.13571, 0.04698, 0.01799};
 
     const std::string config_path = "/home/djole/Master/Thesis/GIT/MT_testing/youbot_driver/config";
     
-    // const std::string urdf_path = "/home/djole/Master/Thesis/GIT/MT_testing/Controller/urdf/youbot_arm_only.urdf";
+   //  const std::string urdf_path = "/home/djole/Master/Thesis/GIT/MT_testing/Controller/urdf/youbot_arm_only.urdf";
     const std::string urdf_path = "/home/djole/Master/Thesis/GIT/MT_testing/Controller/urdf/youbot_arm_zero_inertia.urdf";
-    // const std::string urdf_path = "/home/djole/Master/Thesis/GIT/MT_testing/Controller/urdf/youbot_arm_zero_inertia_new_mass.urdf";
+   //  const std::string urdf_path = "/home/djole/Master/Thesis/GIT/MT_testing/Controller/urdf/youbot_arm_zero_inertia_new_mass.urdf";
+   //  const std::string urdf_path = "/home/djole/Master/Thesis/GIT/MT_testing/Controller/urdf/youbot-ver-azamat.urdf";
 
     const std::string root_name = "arm_link_0";
     const std::string tooltip_name = "arm_link_5";
