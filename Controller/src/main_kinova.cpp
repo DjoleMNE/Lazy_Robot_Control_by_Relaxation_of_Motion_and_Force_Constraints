b14e91dbb758461570a4b037406043e74990ce49
Djordje Vukcevic <vukcevicdjordjemne@gmail.com>
2020-02-28 23:34:50 +0100

    [Main Kinova] Tasks specification added

--- Controller/src/main_kinova.cpp
+++ Controller/src/main_kinova.cpp
@@ -51,7 +51,7 @@ enum path_types
 };
 
 // Waiting time during actions
-const auto ACTION_WAITING_TIME = std::chrono::seconds(1);
+constexpr auto TIMEOUT_DURATION      = std::chrono::seconds{20};
 const int SECOND                     = 1000000;
 const int MILLISECOND                = 1000;
 const int JOINTS                     = 7;
@@ -81,8 +81,8 @@ std::vector<bool> control_dims       = {true, true, true, // Linear
                                         false, false, false}; // Angular
 
 // Last parameter: Numer of points
-const std::vector<double> path_parameters = {0.0, 0.0, 0.0, 0.0, 0};
-std::vector<double> tube_start_position   = {.0, 0.0, 0.0};
+const std::vector<double> path_parameters = {0.0, 0.0, 0.0, 0.0, 90.0};
+std::vector<double> tube_start_position   = {0.0, 0.0, 0.0};
 std::vector<double> tube_tolerances       = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 
 std::vector< std::vector<double> > tube_path_points(path_parameters[4], std::vector<double>(3, 0.0));
@@ -174,33 +174,31 @@ const Eigen::VectorXd gain_step_4           = (Eigen::VectorXd(NUMBER_OF_CONSTRA
                                             << 0.002600, 0.002600, 0.002600, 
                                                0.015152, 0.015152, 0.015152).finished();
 
-const Eigen::VectorXd min_bias_sat                = Eigen::VectorXd::Constant(6, -1.0);
-const Eigen::VectorXd min_command_sat             = Eigen::VectorXd::Constant(6, -1.0);
-const Eigen::VectorXd null_space_abag_parameters  = (Eigen::VectorXd(6) << 0.0, 0.0, 0.0, 0.0, 0.0, 0.0).finished(); // Last param is max command
+const Eigen::VectorXd min_bias_sat               = Eigen::VectorXd::Constant(6, -1.0);
+const Eigen::VectorXd min_command_sat            = Eigen::VectorXd::Constant(6, -1.0);
+const Eigen::VectorXd null_space_abag_parameters = (Eigen::VectorXd(6) << 0.1, 0.1, 0.1, 0.1, 0.1, 0.1).finished(); // Last param is max command
 
 //  Parameters for weight compensation: x_bias-offset (-1.0 <-> 1.0), y_bias-offset, z_bias-offset, K proportional, error-tube(0.0 <-> 1.0),
 //                                      bias-variance, gain-variance, bias slope, 
 //                                      control-period, x_max_trigger_count, y_max_trigger_count, z_max_trigger_count
-const Eigen::VectorXd compensation_parameters = (Eigen::VectorXd(12) \
-                                                << -0.08, -0.07, 0.0, 1.2, 0.015,
-                                                   0.00016, 0.0025, 0.00002,
-                                                   60, 6, 3, 3).finished();
+const Eigen::VectorXd compensation_parameters = (Eigen::VectorXd(12) << -0.08, -0.07, 0.0, 1.2, 0.015,
+                                                                         0.00016, 0.0025, 0.00002,
+                                                                         60, 6, 3, 3).finished();
 
-// Create closure to set finished to true after an END or an ABORT
-std::function<void(Kinova::Api::Base::ActionNotification)> check_for_end_or_abort(bool& finished)
+// Create an event listener that will set the promise action event to the exit value
+// Will set promise to either END or ABORT
+// Use finish_promise.get_future.get() to wait and get the value
+std::function<void(Kinova::Api::Base::ActionNotification)> create_event_listener_by_promise(std::promise<Kinova::Api::Base::ActionEvent>& finish_promise)
 {
-    return [&finished](Kinova::Api::Base::ActionNotification notification)
+    return [&finish_promise] (Kinova::Api::Base::ActionNotification notification)
     {
-        std::cout << "EVENT : " << Kinova::Api::Base::ActionEvent_Name(notification.action_event()) << std::endl;
-
-        // The action is finished when we receive a END or ABORT event
-        switch(notification.action_event())
+        const auto action_event = notification.action_event();
+        switch(action_event)
         {
-            case Kinova::Api::Base::ActionEvent::ACTION_ABORT:
             case Kinova::Api::Base::ActionEvent::ACTION_END:
-                finished = true;
+            case Kinova::Api::Base::ActionEvent::ACTION_ABORT:
+                finish_promise.set_value(action_event);
                 break;
-
             default:
                 break;
         }
@@ -228,7 +226,142 @@ auto lambda_fct_callback = [](const Kinova::Api::Error &err, const Kinova::Api::
     std::cout << serialized_data << std::endl << std::endl;
 };
 
-void run(kinova_mediator &robot_driver)
+int define_task(dynamics_controller *dyn_controller)
+{
+    std::vector<double> desired_ee_pose(12, 0.0);
+
+    // Create End_effector Cartesian Acceleration task
+    dyn_controller->define_ee_acc_constraint(std::vector<bool>{false, false, false, // Linear
+                                                               false, false, false}, // Angular
+                                             std::vector<double>{0.0, 0.0, 0.0, // Linear
+                                                                 0.0, 0.0, 0.0}); // Angular
+    // Create External Forces task
+    dyn_controller->define_ee_external_force(std::vector<double>{0.0, 0.0, 0.0, // Linear
+                                                                 0.0, 0.0, 0.0}); // Angular
+    // Create Feedforward torques task
+    dyn_controller->define_feedforward_torque(std::vector<double>{0.0, 0.0, 0.0, 0.0,
+                                                                  0.0, 0.0, 0.0});
+
+    switch (desired_pose_id)
+    {
+        case desired_pose::CANDLE:
+            tube_start_position = std::vector<double>{0.0, 0.0, 0.0};
+            desired_ee_pose     = { 0.0, 0.0, 0.0, // Linear: Vector
+                                    1.0, 0.0, 0.0, // Angular: Rotation matrix
+                                    0.0, 1.0, 0.0,
+                                    0.0, 0.0, 1.0};
+            break;
+
+        default:
+            // HOME pose
+            tube_start_position = std::vector<double>{0.0, 0.0, 0.0};
+            desired_ee_pose     = { 0.0, 0.0, 0.0, // Linear: Vector
+                                    1.0, 0.0, 0.0, // Angular: Rotation matrix
+                                    0.0, 1.0, 0.0,
+                                    0.0, 0.0, 1.0};
+            break;
+    }
+
+    switch (desired_task_model)
+    {
+        case task_model::moveTo_follow_path:
+            switch (path_type)
+            {
+                case path_types::STEP_PATH:
+                    motion_profile::draw_step_xy(tube_path_points, 6, 0.001,
+                                                 desired_ee_pose[0], desired_ee_pose[1], desired_ee_pose[2]);
+                    break;
+                
+                case path_types::INF_SIGN_PATH:
+                    motion_profile::draw_inf_sign_xy(tube_path_points, 0.5, 0.4, 0.18, 0.5, 
+                                                     desired_ee_pose[0], desired_ee_pose[1], desired_ee_pose[2]);
+                    break;
+
+                case path_types::SINE_PATH:
+                    motion_profile::draw_sine_xy(tube_path_points, path_parameters[0], path_parameters[1],
+                                                 path_parameters[2], path_parameters[3], 
+                                                 desired_ee_pose[0], desired_ee_pose[1], desired_ee_pose[2]);
+                    break;
+                
+                default:
+                    printf("Unsupported path type");
+                    return -1;
+                    break;
+            }
+
+            dyn_controller->define_moveTo_follow_path_task(std::vector<bool>{control_dims[0], control_dims[1], control_dims[2], // Linear
+                                                                             control_dims[3], control_dims[4], control_dims[5]},// Angular
+                                                            tube_path_points,
+                                                            tube_tolerances,
+                                                            tube_speed,
+                                                            1.0, 0.1, //contact_threshold linear and angular
+                                                            task_time_limit_sec,// time_limit
+                                                            control_null_space,
+                                                            desired_null_space_angle,
+                                                            path_poses); // TF pose
+            break;
+
+        case task_model::moveTo:
+            dyn_controller->define_moveTo_task(std::vector<bool>{control_dims[0], control_dims[1], control_dims[2], // Linear
+                                                                 control_dims[3], control_dims[4], control_dims[5]},// Angular
+                                               tube_start_position,
+                                               tube_tolerances,
+                                               tube_speed,
+                                               1.0, 0.1, //contact_threshold linear and angular
+                                               task_time_limit_sec,// time_limit
+                                               control_null_space,
+                                               desired_null_space_angle,
+                                               desired_ee_pose); // TF pose
+            break;
+
+        case task_model::moveGuarded:
+            dyn_controller->define_moveGuarded_task(std::vector<bool>{control_dims[0], control_dims[1], control_dims[2], // Linear
+                                                                      control_dims[3], control_dims[4], control_dims[5]},// Angular
+                                                    tube_start_position,
+                                                    tube_tolerances,
+                                                    tube_speed,
+                                                    1.0, 0.1, //contact_threshold linear and angular
+                                                    task_time_limit_sec,// time_limit
+                                                    control_null_space,
+                                                    desired_null_space_angle,
+                                                    desired_ee_pose); // TF pose
+            break;
+
+        case task_model::moveTo_weight_compensation:
+            dyn_controller->define_moveTo_weight_compensation_task(std::vector<bool>{control_dims[0], control_dims[1], control_dims[2], // Linear
+                                                                                     control_dims[3], control_dims[4], control_dims[5]},// Angular
+                                                                   tube_start_position,
+                                                                   tube_tolerances,
+                                                                   tube_speed,
+                                                                   1.0, 0.1, // contact_threshold linear and angular
+                                                                   task_time_limit_sec,// time_limit
+                                                                   control_null_space,
+                                                                   desired_null_space_angle,
+                                                                   use_mass_alternation,
+                                                                   desired_ee_pose); // TF pose
+            break;
+
+        case task_model::full_pose:
+            dyn_controller->define_desired_ee_pose(std::vector<bool>{control_dims[0], control_dims[1], control_dims[2], // Linear
+                                                                     control_dims[3], control_dims[4], control_dims[5]}, // Angular
+                                                   desired_ee_pose,
+                                                   1.0, 0.2, //contact_threshold linear and angular
+                                                   task_time_limit_sec,
+                                                   control_null_space,
+                                                   desired_null_space_angle,
+                                                   tube_tolerances[7]); // Null space tolerance
+            break;
+
+        default:
+            assert(("Unsupported task model", false));
+            return -1;
+            break;
+    }
+
+    return 0;
+}
+
+void run_test(kinova_mediator &robot_driver)
 {
     int timer_count = 0;
     int64_t now = 0;
@@ -310,16 +443,89 @@ int main(int argc, char **argv)
     control_null_space   = false;
     use_mass_alternation = false;
 
+    kinova_mediator robot_driver;
+    int return_flag = 0;
+    if      (desired_pose_id == desired_pose::HOME)   return_flag = go_to(robot_driver, desired_pose::HOME);
+    else if (desired_pose_id == desired_pose::CANDLE) return_flag = go_to(robot_driver, desired_pose::CANDLE);
+    else return 0;
+
+    if (return_flag != 0) return 0;
+
     // Extract robot model and if not simulation, establish connection with motor drivers
-    robot_driver.initialize(robot_model_id, environment, compensate_gravity);
+    if (!robot_driver.is_initialized()) robot_driver.initialize(robot_model_id, environment, compensate_gravity);
     if (!robot_driver.is_initialized())
     {
         printf("Robot is not initialized\n");
         return 0;
     }
 
-    // if (robot_driver.stop_robot_motion() == -1) return 0;
+    int number_of_segments = robot_driver.get_robot_model().getNrOfSegments();
+    int number_of_joints   = robot_driver.get_robot_model().getNrOfJoints();
+    assert(JOINTS == number_of_segments);
+    state_specification motion(number_of_joints, number_of_segments, number_of_segments + 1, NUMBER_OF_CONSTRAINTS);
+    if (robot_driver.stop_robot_motion() == -1) return 0;
 
-    run(robot_driver);
+    // rotate_joint(robot_driver, 0, 0.1);
+    // robot_driver.get_joint_positions(motion.q);
+    // robot_driver.get_joint_velocities(motion.qd);
+    // std::cout << motion.q << std::endl;
+    // return 0;
+
+    // run_test(robot_driver);
+
+    //loop rate in Hz
+    int rate_hz = 600;
+    dynamics_controller controller(&robot_driver, rate_hz, compensate_gravity);
+
+    int initial_result = define_task(&controller);
+    if (initial_result != 0) return -1;
+
+    if (desired_task_model == task_model::full_pose) 
+    {
+        controller.set_parameters(time_horizon_sec, abag_error_type, 
+                                  max_command, error_alpha,
+                                  bias_threshold, bias_step, gain_threshold,
+                                  gain_step, min_bias_sat, min_command_sat,
+                                  null_space_abag_parameters, compensation_parameters);
+    }
+    else if (desired_task_model == task_model::moveGuarded)
+    {
+        controller.set_parameters(time_horizon_sec, abag_error_type, 
+                                  max_command, error_alpha_1,
+                                  bias_threshold_1, bias_step_1, gain_threshold_1,
+                                  gain_step_1, min_bias_sat, min_command_sat,
+                                  null_space_abag_parameters, compensation_parameters);
+    }
+    else if (desired_task_model == task_model::moveTo_follow_path)
+    {
+        controller.set_parameters(time_horizon_sec, abag_error_type, 
+                                  max_command, error_alpha_3,
+                                  bias_threshold_3, bias_step_3, gain_threshold_3,
+                                  gain_step_3, min_bias_sat, min_command_sat,
+                                  null_space_abag_parameters, compensation_parameters);
+    }
+    else if (desired_task_model == task_model::moveTo_weight_compensation)
+    {
+        controller.set_parameters(time_horizon_sec, abag_error_type, 
+                                  max_command, error_alpha_4,
+                                  bias_threshold_4, bias_step_4, gain_threshold_4,
+                                  gain_step_4, min_bias_sat, min_command_sat,
+                                  null_space_abag_parameters, compensation_parameters);
+    }
+    else
+    {
+        controller.set_parameters(time_horizon_sec, abag_error_type, 
+                                  max_command, error_alpha_2,
+                                  bias_threshold_2, bias_step_2, gain_threshold_2,
+                                  gain_step_2, min_bias_sat, min_command_sat,
+                                  null_space_abag_parameters, compensation_parameters);
+    }
+
+    initial_result = controller.initialize(desired_control_mode, 
+                                           desired_dynamics_interface,
+                                           log_data,
+                                           motion_profile_id);
+    if (initial_result != 0) return -1;
+    controller.control();
     return 0;
 }
