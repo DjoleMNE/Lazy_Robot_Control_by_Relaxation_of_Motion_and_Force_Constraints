eefbad39a6f40c20807aaa258557be22cd219a3a
Djordje Vukcevic <vukcevicdjordjemne@gmail.com>
2020-02-28 23:07:57 +0100

    [CmakeList] minor change

--- Controller/src/main_kinova.cpp
+++ Controller/src/main_kinova.cpp
@@ -34,16 +34,178 @@ SOFTWARE.
 #include <finite_state_machine.hpp>
 #include <motion_profile.hpp>
 
-const long SECOND                    = 1000000;
+#define IP_ADDRESS "192.168.1.10"
+#define PORT 10000
+
+enum desired_pose
+{
+    CANDLE       = 0,
+    HOME         = 1
+};
+
+enum path_types
+{
+    SINE_PATH = 0,
+    STEP_PATH = 1,
+    INF_SIGN_PATH = 2
+};
+
+// Waiting time during actions
+const auto ACTION_WAITING_TIME = std::chrono::seconds(1);
+const int SECOND                     = 1000000;
 const int MILLISECOND                = 1000;
 const int JOINTS                     = 7;
 const int NUMBER_OF_CONSTRAINTS      = 6;
+const int desired_dynamics_interface = dynamics_interface::CART_ACCELERATION;
+const int abag_error_type            = error_type::SIGN;
+int motion_profile_id                = m_profile::CONSTANT;
+int path_type                        = path_types::STEP_PATH;
+int desired_pose_id                  = desired_pose::HOME;
+int desired_task_model               = task_model::full_pose;
+int desired_control_mode             = control_mode::TORQUE;
 int environment                      = kinova_environment::SIMULATION;
 int robot_model_id                   = kinova_model::URDF;
+
+const double time_horizon_sec        = 2.5;
+double tube_speed                    = 0.01;
+double desired_null_space_angle      = 90.0; // Unit degrees
+double task_time_limit_sec           = 600.0;
+double time_duration                 = 3.0f; // Duration of the example (seconds)
+
+const bool log_data                  = true;
+bool control_null_space              = false;
 bool compensate_gravity              = false;
-double time_duration                 = 5.0f; // Duration of the example (seconds)
+bool use_mass_alternation            = false;
 
+std::vector<bool> control_dims       = {true, true, true, // Linear
+                                        false, false, false}; // Angular
 
+// Last parameter: Numer of points
+const std::vector<double> path_parameters = {0.0, 0.0, 0.0, 0.0, 0};
+std::vector<double> tube_start_position   = {.0, 0.0, 0.0};
+std::vector<double> tube_tolerances       = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+
+std::vector< std::vector<double> > tube_path_points(path_parameters[4], std::vector<double>(3, 0.0));
+std::vector< std::vector<double> > path_poses(path_parameters[4] - 1,   std::vector<double>(12, 0.0));
+
+const Eigen::VectorXd max_command         = (Eigen::VectorXd(NUMBER_OF_CONSTRAINTS) << 10.0, 10.0, 10.0, 10.0, 10.0, 10.0).finished();
+
+// Full Pose ABAG parameters
+const Eigen::VectorXd error_alpha         = (Eigen::VectorXd(NUMBER_OF_CONSTRAINTS) \
+                                            << 0.900000, 0.900000, 0.900000, 
+                                               0.850000, 0.850000, 0.850000).finished();
+const Eigen::VectorXd bias_threshold      = (Eigen::VectorXd(NUMBER_OF_CONSTRAINTS) \
+                                            << 0.000407, 0.000407, 0.000407, 
+                                               0.001007, 0.001007, 0.001007).finished();
+const Eigen::VectorXd bias_step           = (Eigen::VectorXd(NUMBER_OF_CONSTRAINTS) \
+                                            << 0.000495, 0.000495, 0.000495, 
+                                               0.003495, 0.003495, 0.003495).finished();
+const Eigen::VectorXd gain_threshold      = (Eigen::VectorXd(NUMBER_OF_CONSTRAINTS) \
+                                            << 0.552492, 0.552492, 0.552492, 
+                                               0.252492, 0.252492, 0.252492).finished();
+const Eigen::VectorXd gain_step           = (Eigen::VectorXd(NUMBER_OF_CONSTRAINTS) \
+                                            << 0.003152, 0.003152, 0.003152, 
+                                               0.015152, 0.015152, 0.015152).finished();
+
+// moveGuarded-torque ABAG parameters
+const Eigen::VectorXd error_alpha_1         = (Eigen::VectorXd(NUMBER_OF_CONSTRAINTS) \
+                                            << 0.850000, 0.900000, 0.900000, 
+                                               0.850000, 0.850000, 0.850000).finished();
+const Eigen::VectorXd bias_threshold_1      = (Eigen::VectorXd(NUMBER_OF_CONSTRAINTS) \
+                                            << 0.000407, 0.000407, 0.000407, 
+                                               0.001007, 0.001007, 0.001007).finished();
+const Eigen::VectorXd bias_step_1           = (Eigen::VectorXd(NUMBER_OF_CONSTRAINTS) \
+                                            << 0.000550, 0.000495, 0.000495, 
+                                               0.003495, 0.003495, 0.003495).finished();
+const Eigen::VectorXd gain_threshold_1      = (Eigen::VectorXd(NUMBER_OF_CONSTRAINTS) \
+                                            << 0.552492, 0.552492, 0.552492, 
+                                               0.252492, 0.252492, 0.252492).finished();
+const Eigen::VectorXd gain_step_1           = (Eigen::VectorXd(NUMBER_OF_CONSTRAINTS) \
+                                            << 0.003152, 0.003152, 0.003152, 
+                                               0.015152, 0.015152, 0.015152).finished();
+
+// moveTo-torque ABAG parameters
+const Eigen::VectorXd error_alpha_2         = (Eigen::VectorXd(NUMBER_OF_CONSTRAINTS) \
+                                            << 0.900000, 0.900000, 0.900000, 
+                                               0.850000, 0.850000, 0.850000).finished();
+const Eigen::VectorXd bias_threshold_2      = (Eigen::VectorXd(NUMBER_OF_CONSTRAINTS) \
+                                            << 0.000457, 0.000407, 0.000407, 
+                                               0.001007, 0.001007, 0.001007).finished();
+const Eigen::VectorXd bias_step_2           = (Eigen::VectorXd(NUMBER_OF_CONSTRAINTS) \
+                                            << 0.000500, 0.000400, 0.000400, 
+                                               0.003495, 0.003495, 0.003495).finished();
+const Eigen::VectorXd gain_threshold_2      = (Eigen::VectorXd(NUMBER_OF_CONSTRAINTS) \
+                                            << 0.502492, 0.502492, 0.502492, 
+                                               0.252492, 0.252492, 0.252492).finished();
+const Eigen::VectorXd gain_step_2           = (Eigen::VectorXd(NUMBER_OF_CONSTRAINTS) \
+                                            << 0.002552, 0.002552, 0.002552, 
+                                               0.015152, 0.015152, 0.015152).finished();
+// moveTo-follow_path-torque ABAG parameters
+const Eigen::VectorXd error_alpha_3         = (Eigen::VectorXd(NUMBER_OF_CONSTRAINTS) \
+                                            << 0.850000, 0.850000, 0.850000, 
+                                               0.850000, 0.850000, 0.850000).finished();
+const Eigen::VectorXd bias_threshold_3      = (Eigen::VectorXd(NUMBER_OF_CONSTRAINTS) \
+                                            << 0.000457, 0.000407, 0.000407, 
+                                               0.001007, 0.001007, 0.001007).finished();
+const Eigen::VectorXd bias_step_3           = (Eigen::VectorXd(NUMBER_OF_CONSTRAINTS) \
+                                            << 0.000550, 0.000550, 0.000550, 
+                                               0.003495, 0.003495, 0.003495).finished();
+const Eigen::VectorXd gain_threshold_3      = (Eigen::VectorXd(NUMBER_OF_CONSTRAINTS) \
+                                            << 0.502492, 0.454092, 0.454092, 
+                                               0.252492, 0.252492, 0.252492).finished();
+const Eigen::VectorXd gain_step_3           = (Eigen::VectorXd(NUMBER_OF_CONSTRAINTS) \
+                                            << 0.003152, 0.003552, 0.003552, 
+                                               0.015152, 0.015152, 0.015152).finished();
+
+// moveTo_weight_compensation-torque ABAG parameters
+const Eigen::VectorXd error_alpha_4         = (Eigen::VectorXd(NUMBER_OF_CONSTRAINTS) \
+                                            << 0.900000, 0.900000, 0.900000, 
+                                               0.850000, 0.850000, 0.850000).finished();
+const Eigen::VectorXd bias_threshold_4      = (Eigen::VectorXd(NUMBER_OF_CONSTRAINTS) \
+                                            << 0.000550, 0.000457, 0.000457, 
+                                               0.001007, 0.001007, 0.001007).finished();
+const Eigen::VectorXd bias_step_4           = (Eigen::VectorXd(NUMBER_OF_CONSTRAINTS) \
+                                            << 0.000500, 0.000450, 0.000450, 
+                                               0.003495, 0.003495, 0.003495).finished();
+const Eigen::VectorXd gain_threshold_4      = (Eigen::VectorXd(NUMBER_OF_CONSTRAINTS) \
+                                            << 0.452492, 0.452492, 0.452492, 
+                                               0.252492, 0.252492, 0.252492).finished();
+const Eigen::VectorXd gain_step_4           = (Eigen::VectorXd(NUMBER_OF_CONSTRAINTS) \
+                                            << 0.002600, 0.002600, 0.002600, 
+                                               0.015152, 0.015152, 0.015152).finished();
+
+const Eigen::VectorXd min_bias_sat                = Eigen::VectorXd::Constant(6, -1.0);
+const Eigen::VectorXd min_command_sat             = Eigen::VectorXd::Constant(6, -1.0);
+const Eigen::VectorXd null_space_abag_parameters  = (Eigen::VectorXd(6) << 0.0, 0.0, 0.0, 0.0, 0.0, 0.0).finished(); // Last param is max command
+
+//  Parameters for weight compensation: x_bias-offset (-1.0 <-> 1.0), y_bias-offset, z_bias-offset, K proportional, error-tube(0.0 <-> 1.0),
+//                                      bias-variance, gain-variance, bias slope, 
+//                                      control-period, x_max_trigger_count, y_max_trigger_count, z_max_trigger_count
+const Eigen::VectorXd compensation_parameters = (Eigen::VectorXd(12) \
+                                                << -0.08, -0.07, 0.0, 1.2, 0.015,
+                                                   0.00016, 0.0025, 0.00002,
+                                                   60, 6, 3, 3).finished();
+
+// Create closure to set finished to true after an END or an ABORT
+std::function<void(Kinova::Api::Base::ActionNotification)> check_for_end_or_abort(bool& finished)
+{
+    return [&finished](Kinova::Api::Base::ActionNotification notification)
+    {
+        std::cout << "EVENT : " << Kinova::Api::Base::ActionEvent_Name(notification.action_event()) << std::endl;
+
+        // The action is finished when we receive a END or ABORT event
+        switch(notification.action_event())
+        {
+            case Kinova::Api::Base::ActionEvent::ACTION_ABORT:
+            case Kinova::Api::Base::ActionEvent::ACTION_END:
+                finished = true;
+                break;
+
+            default:
+                break;
+        }
+    };
+}
 
 /*****************************
  * Example related function *
@@ -79,7 +241,7 @@ void run(kinova_mediator &robot_driver)
     jnt_array_command(3) = 0.0;
     jnt_array_command(4) = 0.0;
     jnt_array_command(5) = 0.0;
-    jnt_array_command(6) = 0.2;
+    jnt_array_command(6) = 0.5;
 
     if (robot_driver.set_control_mode(control_mode::VELOCITY) == -1)
     {
@@ -117,14 +279,37 @@ void run(kinova_mediator &robot_driver)
     printf("Task completed\n");
 }
 
+void rotate_joint(kinova_mediator &robot_driver, const int joint, const double rate)
+{
+    robot_driver.set_control_mode(control_mode::VELOCITY);
+    KDL::JntArray rotate_joint(JOINTS);
+    rotate_joint(joint) = rate; 
+    robot_driver.set_joint_velocities(rotate_joint);
+    if (environment != kinova_environment::SIMULATION) usleep(5000 * MILLISECOND);
+}
+
 int main(int argc, char **argv)
 {
     printf("kinova MAIN Started \n");
-    kinova_mediator robot_driver;
-    environment        = kinova_environment::SIMULATION;
-    robot_model_id     = kinova_model::URDF;
-    compensate_gravity = false;
-    
+    control_dims         = std::vector<bool>{true, true, true, // Linear
+                                             false, false, false}; // Angular
+    tube_tolerances      = std::vector<double>{0.001, 0.01, 0.01, 
+                                               0.0, 0.0, 0.0, 
+                                               0.005, 0.0}; // Last tolerance is in unit of degrees - Null-space tolerance
+    environment          = kinova_environment::SIMULATION;
+    robot_model_id       = kinova_model::URDF;
+    desired_pose_id      = desired_pose::HOME;
+    desired_control_mode = control_mode::TORQUE;
+    desired_task_model   = task_model::moveTo;
+    // desired_task_model   = task_model::full_pose;
+    path_type            = path_types::STEP_PATH;
+    motion_profile_id    = m_profile::CONSTANT;
+    task_time_limit_sec  = 10.5;
+    tube_speed           = 0.01;
+    compensate_gravity   = false;
+    control_null_space   = false;
+    use_mass_alternation = false;
+
     // Extract robot model and if not simulation, establish connection with motor drivers
     robot_driver.initialize(robot_model_id, environment, compensate_gravity);
     if (!robot_driver.is_initialized())
